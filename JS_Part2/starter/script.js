7391638939d7c9f5f767d24a099979dcd6590366
Siddhartha Upadhyay <siddharthaupadhyay@Siddharthas-MacBook-Air.local>
2023-05-30 02:26:27 +0530

    Phase 2 Js fundamentals

---
+++ JS_Part2/starter/script.js
@@ -0,0 +1,10 @@
+'use script';
+function juiceMixer(apple, oranges) {
+    const juice = `The juice is getting made with ${apple} apples and ${oranges} oranges.`;
+    console.log(juice);
+}
+
+
+juiceMixer(1, 6);
+juiceMixer(0, 4);
+juiceMixer(5, 6);
