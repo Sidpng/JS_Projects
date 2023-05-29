460d476b3bbb63cd7abd43038ef7a2f5e48ef951
Siddhartha Upadhyay <siddharthaupadhyay@Siddharthas-MacBook-Air.local>
2023-05-30 03:11:42 +0530

    Phase 2 Js fundamentals

--- JS_Part2/starter/script.js
+++ JS_Part2/starter/script.js
@@ -1,10 +1,16 @@
 'use script';
-function juiceMixer(apple, oranges) {
-    const juice = `The juice is getting made with ${apple} apples and ${oranges} oranges.`;
-    console.log(juice);
+// function juiceMixer(apple, oranges) {
+//     const juice = `The juice is getting made with ${apple} apples and ${oranges} oranges.`;
+//     console.log(juice);
+// }
+
+
+// juiceMixer(1, 6);
+// juiceMixer(0, 4);
+// juiceMixer(5, 6);
+
+function calcAge(birthYear) {
+    return 2023 - birthYear;
 }
 
-
-juiceMixer(1, 6);
-juiceMixer(0, 4);
-juiceMixer(5, 6);
+console.log("Age of person born in 1994 : " + calcAge(1994));
