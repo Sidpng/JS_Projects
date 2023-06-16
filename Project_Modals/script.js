4fb6db4ee943f032e113ba5b1bde86eafd58708a
Siddhartha Upadhyay <siddharthaupadhyay@Siddharthas-MacBook-Air.local>
2023-06-17 03:09:17 +0530

    Added first few lines for project number 2

--- Project_Modals/script.js
+++ Project_Modals/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+    // can select each element within querySelectAll one by one to handle each matching element individually
+}
